8d62496198e6888e3a9614f003bbd48df5dee211
Neelam Verma <neelamv222@gmail.com>
2020-08-16 18:21:05 +0200

    initial commit: project setup, screens, unit test cases added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import WeatherInfo from "./weather-info/weather-info";
 
-function App() {
+import "./app.css"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__container">
+      <WeatherInfo />
     </div>
-  );
+  )
 }
-
 export default App;
