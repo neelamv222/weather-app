b493f91c451f6176ce62230b09f5f34f237b41c0
Neelam Verma <neelamv222@gmail.com>
2020-10-26 21:01:47 +0100

    added initial.js

---
+++ src/initial.js
@@ -0,0 +1,11 @@
+import React from "react";
+import WeatherInfo from "./weather-info/weather-info";
+
+const Initial = () => {
+  return (
+    <div className="app__container">
+      <WeatherInfo />
+    </div>
+  )
+}
+export default Initial;
